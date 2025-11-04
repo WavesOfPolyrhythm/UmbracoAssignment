7fdc09378fe96dd7b0701aa01dded4ff074a3551
WavesOfPolyRhythm <tim.newton.larsson@outlook.com>
2025-11-04 17:13:11 +0100

    Added form submission functionality with controller, service and DI setup. Form submissions now works in backoffice

--- UmbracoOnatrix/Controllers/FormController.cs
+++ UmbracoOnatrix/Controllers/FormController.cs
@@ -6,20 +6,26 @@ using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoOnatrix.Services;
 using UmbracoOnatrix.ViewModels;
 namespace UmbracoOnatrix.Controllers;
 
-public class FormController : SurfaceController
+public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissionsService formSubmissionsService) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
 {
-    public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
-    {
-    }
+    private readonly FormSubmissionsService _formSubmissionsService = formSubmissionsService;
 
     public IActionResult HandleCallbackForm(CallbackFormViewModel model)
     {
         if (!ModelState.IsValid)
         {
             return CurrentUmbracoPage();
+        }
+
+        var result = _formSubmissionsService.SaveCallbackRequest(model);
+        if (!result)
+        {
+            ModelState.AddModelError("", "There was an error saving your request. Please try again.");
+            return RedirectToCurrentUmbracoPage();
         }
 
         // Work with form data here
